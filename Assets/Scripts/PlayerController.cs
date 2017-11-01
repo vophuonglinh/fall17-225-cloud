5cc881af26e3f2ab29c1077f19ff3360621b5352
Leqi Zhao <leqizhao@Leqis-MacBook-Pro.local>
2017-11-01 10:33:41 -0500

    created collision with cloud in PlayerController, addded GameController class and game object, tagged left tile and right tile cloud as cloud, add README file, increase player sphere speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,15 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 	public float speed;
+	public GameController gamecontroller;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		gamecontroller = gameControllerObject.GetComponent <GameController>();
+		Debug.Log ("Start!");
+
 	}
 	void Update ()
 	{
@@ -22,4 +28,20 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
 	}
+
+	// collision with the clouds
+
+	//void onTriggerEnter(Collider other) {
+	void OnParticleCollision(GameObject other){
+		if (other.gameObject.CompareTag("Cloud")) {
+			Debug.Log ("collided!");
+			gamecontroller.GameOver();
+			gamecontroller.DeleteAll ();
+			Debug.Log ("Game Over!");
+			//Destroy(other.gameObject);
+			//Destroy(gameObject);
+		}
+	}
+		
+
 }
