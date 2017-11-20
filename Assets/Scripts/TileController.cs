2b03437cfe1bb85817082cfd7ac97f91f37379c8
Elena Smith <esmith12@macalester.edu>
2017-11-19 22:53:34 -0600

    move clouds off path

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,54 +1,58 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TileController : MonoBehaviour {
+public class TileController : MonoBehaviour
+{
 
-	public GameObject currentTile;
-	public GameObject[] tilePrefabs;
+    public GameObject currentTile;
+    public GameObject[] tilePrefabs;
 
-	private static TileController instance;
+    private static TileController instance;
     private const string tagCur = "CurrentTile";
     private const string tagNotCur = "NotCurrent";
     private Stack<GameObject> leftTiles = new Stack<GameObject>();
-	private Stack<GameObject> topTiles = new Stack<GameObject>();
-	private Stack<GameObject> rightTiles = new Stack<GameObject>();
+    private Stack<GameObject> topTiles = new Stack<GameObject>();
+    private Stack<GameObject> rightTiles = new Stack<GameObject>();
 
-	private int minSegmentLength;
-	private List<char> tileHistory;
+    private int minSegmentLength;
+    private List<char> tileHistory;
 
 
     //Instance so that TileScript can access inside TileScript
-	public static TileController Instance
-	{
-		get 
-		{ 
-			if (instance == null) {
-				instance = GameObject.FindObjectOfType<TileController> ();
-			}
-			return instance;
-		}
-	}
-
-	void Start () {
-		minSegmentLength = 5;
-		tileHistory = new List<char> (minSegmentLength);
-		for (int i = 0; i < minSegmentLength; i++) 
-		{
-            tileHistory.Add('t'); //initialize tile history with 't' for top tile
-		}
-		CreateTiles (minSegmentLength, 't');
-		for (int i = 0; i < minSegmentLength; i++) {
-            SpawnTile ("top");
+    public static TileController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TileController>();
+            }
+            return instance;
         }
-	}
+    }
 
-	public void CreateTiles(int amount, char type)
-	{
+    void Start()
+    {
+        minSegmentLength = 5;
+        tileHistory = new List<char>(minSegmentLength);
+        for (int i = 0; i < minSegmentLength; i++)
+        {
+            tileHistory.Add('t'); //initialize tile history with 't' for top tile
+        }
+        CreateTiles(minSegmentLength, 't');
+        for (int i = 0; i < minSegmentLength; i++)
+        {
+            SpawnTile("top");
+        }
+    }
+
+    public void CreateTiles(int amount, char type)
+    {
         if (type == 'l')
         {
             for (int i = 0; i < amount; i++)
             {
-			    leftTiles.Push(Instantiate(tilePrefabs[0]));
+                leftTiles.Push(Instantiate(tilePrefabs[0]));
                 leftTiles.Peek().name = "LeftTile";
                 leftTiles.Peek().SetActive(false);
             }
@@ -71,45 +75,47 @@ public class TileController : MonoBehaviour {
                 rightTiles.Peek().SetActive(false);
             }
         }
-	}
+    }
 
-	public Stack<GameObject> LeftTiles
-	{
-		get { return leftTiles;}
-		set { leftTiles = value;}
-	}
-	public Stack<GameObject> TopTiles
-	{
-		get { return topTiles;}
-		set { topTiles = value;}
-	}
-	public Stack<GameObject> RightTiles
-	{
-		get { return rightTiles;}
-		set { rightTiles = value;}
-	}
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Stack<GameObject> LeftTiles
+    {
+        get { return leftTiles; }
+        set { leftTiles = value; }
+    }
+    public Stack<GameObject> TopTiles
+    {
+        get { return topTiles; }
+        set { topTiles = value; }
+    }
+    public Stack<GameObject> RightTiles
+    {
+        get { return rightTiles; }
+        set { rightTiles = value; }
+    }
+    // Update is called once per frame
+    void Update()
+    {
 
-	public void SpawnTile(string preferredDirection="")
-	{
+    }
+
+    public void SpawnTile(string preferredDirection = "")
+    {
         checkEmpty();
         string direction = "top";
-		bool foundTurn = false;
-		int childIndex = 1;
+        bool foundTurn = false;
+        int childIndex = 1;
         char lastMove = tileHistory[0];
 
-		for (int i = 0; i < minSegmentLength; i++) 
-		{
-			if (tileHistory [i] != lastMove) 
-			{
-				foundTurn = true;
-			}
-		}
+        for (int i = 0; i < minSegmentLength; i++)
+        {
+            if (tileHistory[i] != lastMove)
+            {
+                foundTurn = true;
+            }
+        }
 
-		if (foundTurn) {
+        if (foundTurn)
+        {
             if (lastMove == 't')
             {
                 childIndex = 1;
@@ -120,36 +126,38 @@ public class TileController : MonoBehaviour {
                 childIndex = 0;
                 direction = "left";
             }
-            else if (lastMove == 'r'){
+            else if (lastMove == 'r')
+            {
                 childIndex = 2;
                 direction = "right";
             }
 
-		}
+        }
 
         else
         {
-			int randomDirectionIndex = Random.Range (0, tilePrefabs.Length); 
-			string randomDirectionTilePrefab = tilePrefabs [randomDirectionIndex].name;
+            int randomDirectionIndex = Random.Range(0, tilePrefabs.Length);
+            string randomDirectionTilePrefab = tilePrefabs[randomDirectionIndex].name;
 
-			switch (randomDirectionTilePrefab) {
-				case "LeftTile1":
-					childIndex = 0;
-					direction = "left";
-					break;
-				case "RightTile1":
-					childIndex = 2;
-					direction = "right";
-					break;
-				case "TopTile1":
-					childIndex = 1;
-					direction = "top";
-					break;
-				default:
-					childIndex = 1;
-					break;
-			}
-		}
+            switch (randomDirectionTilePrefab)
+            {
+                case "LeftTile1":
+                    childIndex = 0;
+                    direction = "left";
+                    break;
+                case "RightTile1":
+                    childIndex = 2;
+                    direction = "right";
+                    break;
+                case "TopTile1":
+                    childIndex = 1;
+                    direction = "top";
+                    break;
+                default:
+                    childIndex = 1;
+                    break;
+            }
+        }
         if (preferredDirection.Length != 0)
         {
             if (preferredDirection == "top")
@@ -160,43 +168,70 @@ public class TileController : MonoBehaviour {
         }
 
         RecycleTile(direction, childIndex);
-	}
+    }
 
     public void RecycleTile(string direction, int childIndex)
     {
+        GameObject temp;
         if (direction == "left")
         {
             tileHistory.Insert(0, 'l');
             tileHistory.RemoveAt(tileHistory.Count - 1);
-            GameObject temp = leftTiles.Pop();
-            temp.SetActive(true);
-            temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(childIndex).position;
-            currentTile = temp;
-            temp.gameObject.tag = tagCur;
+            temp = leftTiles.Pop();
         }
         else if (direction == "top")
         {
             tileHistory.Insert(0, 't');
             tileHistory.RemoveAt(tileHistory.Count - 1);
-            GameObject temp = topTiles.Pop();
-            temp.SetActive(true);
-            temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(childIndex).position;
-            currentTile = temp;
-            temp.gameObject.tag = tagCur;
+            temp = topTiles.Pop();
         }
-        else if (direction == "right")
+        else
         {
             tileHistory.Insert(0, 'r');
             tileHistory.RemoveAt(tileHistory.Count - 1);
-            GameObject temp = rightTiles.Pop();
-            temp.SetActive(true);
-            temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(childIndex).position;
-            currentTile = temp;
-            temp.gameObject.tag = tagCur;
+            temp = rightTiles.Pop();
         }
+        temp.SetActive(true);
+        temp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(childIndex).position;
+
+
+        currentTile = temp;
+        temp.gameObject.tag = tagCur;
+
+
     }
 
-    void checkEmpty() {
+    public void checkHideCloud() {
+        GameObject[] clouds = GameObject.FindGameObjectsWithTag("Cloud");
+        Vector3 tilePos = currentTile.transform.position;
+        foreach (GameObject cloud in clouds)
+        {
+            Vector3 cloudPos = cloud.transform.position;
+
+            var xDiff = Mathf.Abs(cloudPos.x - tilePos.x);
+            var zDiff = Mathf.Abs(cloudPos.z - tilePos.z);
+            //Debug.Log(string.Format("{0}, {1}", "xDiff ", xDiff.ToString()));
+            //Debug.Log(string.Format("{0}, {1}", "zDiff ", zDiff.ToString()));
+
+            //Debug.Log(string.Format("{0}, {1}", "cloud Pos ", cloudPos.ToString()));
+            //Debug.Log(string.Format("{0}, {1}", "tile Pos ", tilePos.ToString()));
+
+
+            if (xDiff < 70 && zDiff < 70)
+            {
+                //Debug.Log(string.Format("{0}, {1}", "xDiff hit ", xDiff.ToString()));
+                //Debug.Log(string.Format("{0}, {1}", "zDiff hit ", zDiff.ToString()));
+
+                cloud.transform.Rotate(new Vector3(45, 0, 45));
+                cloud.SetActive(false);
+                
+            }
+        }
+
+}
+
+    void checkEmpty()
+    {
         if (leftTiles.Count == 0)
         {
             CreateTiles(10, 'l');
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -5,34 +5,21 @@ using UnityEngine;
 public class TileScript : MonoBehaviour {
 
 	private float delay = 6;
-    private GameObject player;
     private float cloudHeight;
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        GameObject cloud = GameObject.FindGameObjectWithTag("Cloud");
-        float cloudHeight = cloud.transform.lossyScale.y;
-        string message = "Cloud height is ";
-        message = string.Format("{0} {1}", message, cloudHeight.ToString());
-        Debug.Log(message);
-
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 playerPosition = gameObject.transform.position;
-        Vector3 tilePosition = gameObject.transform.position;
-        //compare these positions and if player is in the x,z position range of the tile, then recycle
-        Vector3 tileWidth = gameObject.transform.lossyScale;
-
     }
 		
 	void OnTriggerExit(Collider other)
 	{
-        //TODO: ELENA: rewrite to instead use x and z position to detect recycle
-        Debug.Log("on trigger exit happens");
+        //Debug.Log("on trigger exit happens");
 		if (other.tag == "Player") {
-            Debug.Log("on trigger exit happens");
+            //Debug.Log("on trigger exit happens");
             TileController.Instance.SpawnTile ();
 			StartCoroutine (Recycle());
 		}
@@ -46,7 +33,8 @@ public class TileScript : MonoBehaviour {
 //		yield return new WaitForSeconds(fallDelay);
 		//GetComponent<Rigidbody>.isKinematic = false;
 		yield return new WaitForSeconds(delay);
-		switch (gameObject.name)
+		TileController.Instance.checkHideCloud();
+        switch (gameObject.name)
 		{
 		case "LeftTile":
 			TileController.Instance.LeftTiles.Push(gameObject);  // add it back to the tile stack to recycle it
@@ -61,5 +49,7 @@ public class TileScript : MonoBehaviour {
 			//Debug.Log("pushed right tile back to stack");
 			break;
 		}
+
+
 	}
 }
