065d2e69dbb2c1846e9a2e1e0c77c5c442d5f456
vophuonglinh <lvo1@macalester.edu>
2017-11-28 22:07:36 -0600

    Set up keyboard control
    1) Press any key to start
    2) Use arrow keys to move around, it works pretty much similarly to swiping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     private bool isStarted = false;     // test if game has started
+
     private int inCloudTime = 0;
     public ParticleSystem sparkleEffect;
     public GameController gamecontroller;
+
     private int count;
     private int life = 3;
     public Text countText;
@@ -22,6 +24,7 @@ public class PlayerController : MonoBehaviour
     public Text lifeText;
     private const int ALLOWED_IN_CLOUD_TIME = 500;
     private const int SPARKLE_POS_OFFSET_X = 50;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -38,33 +41,31 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
-        if (Input.touchCount > 0)
+      if (!isStarted)
+      {
+        if (Input.anyKey || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
             {
-                if (!isStarted)
-                {                   // if game hasn't started, player touch screen
-                    rb.AddForce(new Vector3(0.0f, 0.0f, 100f) * speed);
-                    isStarted = true;
-                }
+              rb.AddForce(new Vector3(0.0f, 0.0f, 100f) * speed);
+              isStarted = true;
             }
         }
-        if (life <= 0)
-        {
-            endGame();
-        }
-    }
+      }
 
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+      else {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+			       transform.position += Vector3.right * speed * Time.deltaTime;
+		    }
+		    if (Input.GetKey(KeyCode.LeftArrow)){
+			       transform.position += Vector3.left* speed * Time.deltaTime;
+		    }
+		    if (Input.GetKey(KeyCode.UpArrow)){
+			       transform.position += Vector3.up * speed * Time.deltaTime;
+		    }
+		    if (Input.GetKey(KeyCode.DownArrow)){
+			       transform.position += Vector3.down* speed * Time.deltaTime;
+		    }
+      }
     }
 
 
@@ -119,7 +120,7 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Cloud"))
         {
             //TODO the + or - 50 should be based on the direction of swipe
-            Vector3 sparklePosition = new Vector3(gameObject.transform.position.x - SPARKLE_POS_OFFSET_X, 
+            Vector3 sparklePosition = new Vector3(gameObject.transform.position.x - SPARKLE_POS_OFFSET_X,
                 gameObject.transform.position.y,gameObject.transform.position.z);
             sparkleEffect.transform.position = sparklePosition;
             sparkleEffect.Play();
