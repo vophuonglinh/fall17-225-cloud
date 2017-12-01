96e582c0c336940a70ffd6977c9f4ce2aadb65bc
Elena Smith <esmith12@macalester.edu>
2017-12-01 15:51:23 -0600

    boost player velocity upon hitting boost
    add force in update to player's rigid body after count of boost is incremented.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     public GameController gamecontroller;
 
     private int count;
+    private int lastCount;
     private int life = 3;
     public Text countText;
     public Text highScore;
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        lastCount = count;
         SetCountText();
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
         gamecontroller = gameControllerObject.GetComponent<GameController>();
@@ -41,31 +43,44 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-      if (!isStarted)
-      {
-        if (Input.anyKey || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (lastCount != count)
         {
+            //boost the player speed!
+            float boost = 100f;
+            rb.AddForce(rb.velocity.normalized * boost * Time.deltaTime, ForceMode.Impulse);
+            Debug.Log(rb.velocity.magnitude);
+            lastCount = count;
+        }
+        if (!isStarted)
+        {
+            if (Input.anyKey || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-              rb.AddForce(new Vector3(0.0f, 0.0f, 100f) * speed);
-              isStarted = true;
+
+                rb.AddForce(new Vector3(0.0f, 0.0f, 100f) * speed);
+                isStarted = true;
+
             }
         }
-      }
 
-      else {
-        if (Input.GetKey(KeyCode.RightArrow)) {
-			       transform.position += Vector3.right * speed * Time.deltaTime;
-		    }
-		    if (Input.GetKey(KeyCode.LeftArrow)){
-			       transform.position += Vector3.left* speed * Time.deltaTime;
-		    }
-		    if (Input.GetKey(KeyCode.UpArrow)){
-			       transform.position += Vector3.up * speed * Time.deltaTime;
-		    }
-		    if (Input.GetKey(KeyCode.DownArrow)){
-			       transform.position += Vector3.down* speed * Time.deltaTime;
-		    }
-      }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
     }
 
 
@@ -121,18 +136,20 @@ public class PlayerController : MonoBehaviour
         {
             //TODO the + or - 50 should be based on the direction of swipe
             Vector3 sparklePosition = new Vector3(gameObject.transform.position.x - SPARKLE_POS_OFFSET_X,
-                gameObject.transform.position.y,gameObject.transform.position.z);
+                gameObject.transform.position.y, gameObject.transform.position.z);
             sparkleEffect.transform.position = sparklePosition;
             sparkleEffect.Play();
             Debug.Log("collided cloud!");
-            if (inCloudTime >= ALLOWED_IN_CLOUD_TIME) {
+            if (inCloudTime >= ALLOWED_IN_CLOUD_TIME)
+            {
                 endGame();
                 Debug.Log("Game Over!");
             }
         }
     }
 
-    void endGame(){
+    void endGame()
+    {
         gamecontroller.GameOver();
 
     }
